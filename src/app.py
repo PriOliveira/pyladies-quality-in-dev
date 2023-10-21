bdba3765d29a4b5c0034083c0e81c89cb77301ce
Priscila Oliveira <priscila.oliveira@celus.io>
2023-10-21 23:19:03 +0200

    fix: remaining linting issues, bug on fahrenheit_to_celsius and add more tests

--- src/app.py
+++ src/app.py
@@ -1,40 +1,43 @@
-from typing import Literal
+"""Temperature conversion."""
+
+import logging
+from collections.abc import Callable
+from typing import Literal, cast
 
 TemperatureUnit = Literal["K", "C", "F"]
 
 
 def celsius_to_fahrenheit(temperature_celsius: float) -> float:
-    """Converts Celsius to Fahrenheit."""
-    temperature_fahrenheit = temperature_celsius * 9 / 5 + 32
-    return temperature_fahrenheit
+    """Convert Celsius to Fahrenheit."""
+    return temperature_celsius * 9 / 5 + 32
 
 
 def celsius_to_kelvin(temperature_celsius: float) -> float:
-    """Converts Celsius to Kelvin."""
+    """Convert Celsius to Kelvin."""
     return temperature_celsius + 273.15
 
 
 def fahrenheit_to_celsius(temperature_fahrenheit: float) -> float:
-    """Converts Fahrenheit to Celsius."""
-    return (temperature_fahrenheit - 32) + 5 / 9
+    """Convert Fahrenheit to Celsius."""
+    return (temperature_fahrenheit - 32) * 5 / 9
 
 
 def fahrenheit_to_kelvin(temperature_fahrenheit: float) -> float:
-    """Converts Fahrenheit to Kelvin."""
+    """Convert Fahrenheit to Kelvin."""
     return (temperature_fahrenheit + 459.67) * 5 / 9
 
 
 def kelvin_to_celsius(temperature_kelvin: float) -> float:
-    """Converts Kelvin to Celsius."""
+    """Convert Kelvin to Celsius."""
     return temperature_kelvin - 273.15
 
 
 def kelvin_to_fahrenheit(temperature_kelvin: float) -> float:
-    """Converts Kelvin to Fahrenheit."""
+    """Convert Kelvin to Fahrenheit."""
     return temperature_kelvin * 9 / 5 - 459.67
 
 
-conversion_fn_lut = {
+conversion_fn_lut: dict[TemperatureUnit, dict[TemperatureUnit, Callable[[float], float]]] = {
     "C": {
         "F": celsius_to_fahrenheit,
         "K": celsius_to_kelvin,
@@ -55,16 +58,29 @@ def convert_temperature(
     unit_to: TemperatureUnit,
     temperature: float,
 ) -> float:
-    """Converts a temperature from one unit into another unit."""
+    """Convert a temperature from one unit into another unit."""
     conversion_fn = conversion_fn_lut[unit_from][unit_to]
     return conversion_fn(temperature)
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+
     unit_from = input("Convert from (K, F or C):")
     unit_to = input("Convert to (K, F or C):")
     temperature = input("Value to be converted: ")
 
-    final_temperature = convert_temperature(unit_from, unit_to, float(temperature))
-
-    print(f"Converted {temperature} {unit_from} to {final_temperature} {unit_to}")
+    final_temperature = convert_temperature(
+        cast(TemperatureUnit, unit_from),
+        cast(TemperatureUnit, unit_to),
+        float(temperature),
+    )
+    data = {
+        "temperature": temperature,
+        "unit_from": unit_from,
+        "final_temperature": final_temperature,
+        "unit_to": unit_to,
+    }
+    template = "Converted successfully: %(temperature)s%(unit_from)s to %(final_temperature).2f%(unit_to)s"
+    message = template % data
+    logging.info(message)
--- tests/test_app.py
+++ tests/test_app.py
@@ -4,32 +4,36 @@ from src import app as converter
 
 
 @pytest.mark.parametrize(
-    "value, expected",
+    ("value", "expected"),
     [
         (0, 273.15),
         (100, 373.15),
     ],
 )
-def test_celsius_to_kelvin(value: float, expected: float):
+def test_celsius_to_kelvin(value: float, expected: float) -> None:
     assert converter.celsius_to_kelvin(value) == expected
 
 
 @pytest.mark.parametrize(
-    "value, expected",
+    ("value", "expected"),
     [
         (0, 32),
         (100, 212),
     ],
 )
-def test_celsius_to_fahrenheit(value: float, expected: float):
+def test_celsius_to_fahrenheit(value: float, expected: float) -> None:
     assert converter.celsius_to_fahrenheit(value) == expected
 
 
 @pytest.mark.parametrize(
-    "unit_from, unit_to, temperature, expected",
+    ("unit_from", "unit_to", "temperature", "expected"),
     [
-        ("C", "F", 100, 212),
-        ("C", "K", 0, 273.15),
+        pytest.param("C", "F", 100, 212, id="celsius_to_fahrenheit"),
+        pytest.param("F", "C", 212, 100, id="fahrenheit_to_celsius"),
+        pytest.param("C", "K", 0, 273.15, id="celsius_to_kelvin"),
+        pytest.param("K", "C", 273.15, 0, id="kelvin_to_celsius"),
+        pytest.param("K", "F", 273.15, 32, id="kelvin_to_fahrenheit"),
+        pytest.param("F", "K", 32, 273.15, id="fahrenheit_to_kelvin"),
     ],
 )
 def test_convert_temperature(
@@ -37,10 +41,10 @@ def test_convert_temperature(
     unit_to: converter.TemperatureUnit,
     temperature: float,
     expected: float,
-):
+) -> None:
     result = converter.convert_temperature(
         unit_from=unit_from,
         unit_to=unit_to,
         temperature=temperature,
     )
-    assert result == expected
+    assert pytest.approx(result, 0.1) == expected
